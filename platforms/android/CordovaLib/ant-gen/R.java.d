7c9396b6a1f0e6cecf22f6953e8e0d6d5cf86686
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-03-02 20:13:17 +0530

    code cleanup

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/AndroidManifest.xml : \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/AndroidManifest.xml \
