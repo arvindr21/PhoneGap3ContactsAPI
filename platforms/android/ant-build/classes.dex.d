4be87b1c766a2b0cc8b45fe05852411166dc68d9
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-03-02 14:46:41 +0530

    removed android folder

--- platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes.dex : \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/BuildConfig.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/R$attr.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/R$drawable.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/R$string.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/R$xml.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/R.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/classes/com/app/SearchContactsAPI/SearchContactsAPI.class \
-/Applications/MAMP/htdocs/PG3ContactsAPI/PhoneGap3ContactsAPI/platforms/android/ant-build/dexedLibs/classes-a8b5f8bcdaaee1ca35aa52271fc89f46.jar \
