ab68371e26d23ee843c1b03406897a6d631f379c
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-03-02 18:26:23 +0530

    Added Contacts API

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,81 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+function onDeviceReady() {
+    if ($("#contactsList").length == 1) {
+        $("body").addClass('ui-disabled').css("background", "#000");
+        $.mobile.loading("show");
+        var options = new ContactFindOptions();
+        options.filter = "";
+        options.multiple = true;
+        var filter = ["displayName", "phoneNumbers"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
+    } else if ($("#addContact").length == 1) {
+        bindAddContactEvents();
     }
-};
+}
+
+function onSuccess(contacts) {
+    var html = "";
+    for (var i = 0; i < contacts.length; i++) {
+        if ($.trim(contacts[i].displayName).length != 0) {
+            html += '<li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">';
+            html += '<h2>' + contacts[i].displayName + '</h2>';
+            html += '<h3>Phone Numbers</h3>';
+            for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+                html += "Type: " + contacts[i].phoneNumbers[j].type + "\n" +
+                    "Value: " + contacts[i].phoneNumbers[j].value + "\n" +
+                    "Preferred: " + contacts[i].phoneNumbers[j].pref + "</hr>";
+            }
+            html += '</li>';
+        }
+    }
+    $("#contactsList").html(html);
+    $("#contactsList").listview('refresh');
+    $.mobile.loading("hide");
+    $("body").removeClass('ui-disabled');
+}
+
+function onError(contactError) {
+    alert('Oops Something went wrong!');
+    $.mobile.loading("hide");
+    $("body").removeClass('ui-disabled');
+}
+
+function bindAddContactEvents() {
+    $("#addContact").on("click", function () {
+        var name = $.trim($("#name").val()),
+            number = $.trim($("#number").val());
+
+        if (name.length == 0) {
+            alert("Please enter a valid Name");
+            return false;
+        }
+
+        if (number.length == 0) {
+            alert("Please enter a valid Number");
+            return false;
+        }
+
+        var contact = navigator.contacts.create();
+        contact.displayName = name;
+        contact.nickname = name;
+
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('mobile', number, true);
+        contact.save(createSuccess, createError);
+    });
+}
+
+function createSuccess() {
+    alert("Contact has been successfully added");
+    resetPage();
+}
+
+function createError() {
+    alert("Oops Something went wrong! Please try again later.");
+}
+
+function resetPage() {
+    $("#name").val("");
+    $("#number").val("");
+}
